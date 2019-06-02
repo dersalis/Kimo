c1fb425e74dcb871420df09b1085cbb56ac786c9
dersalis <dersalis@outlook.com>
2019-06-02 22:20:14 +0200

    Nawigacja

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'start', component: StartComponent},
+  { path: '', redirectTo: 'start', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
