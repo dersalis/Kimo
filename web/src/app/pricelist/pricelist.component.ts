8b8449825af21c2bc8a541004a1b117fc84345f4
dersalis <dersalis@outlook.com>
2019-06-05 23:00:46 +0200

    Cennik

--- web/src/app/pricelist/pricelist.component.ts
+++ web/src/app/pricelist/pricelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IService } from '../models/IService';
 
 @Component({
   selector: 'app-pricelist',
@@ -6,6 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricelist.component.scss']
 })
 export class PricelistComponent implements OnInit {
+
+  public ticets: IService[] = [
+    {
+      title: '2D',
+      type: 'Ulgowy',
+      price: 20,
+    },
+    {
+      title: '2D',
+      type: 'Normalny',
+      price: 30,
+    },
+    {
+      title: '3D',
+      type: 'Ulgowy',
+      price: 25,
+    },
+    {
+      title: '3D',
+      type: 'Normalny',
+      price: 35,
+    },
+  ];
+
+  public dishes: IService[] = [
+    {
+      title: 'Popcorn',
+      type: 'Mały',
+      price: 10,
+    },
+    {
+      title: 'Popcorn',
+      type: 'Duży',
+      price: 15,
+    },
+    {
+      title: 'Nachos',
+      type: 'Mały',
+      price: 10,
+    },
+    {
+      title: 'Nachos',
+      type: 'Duży',
+      price: 15,
+    },
+    {
+      title: 'Napój',
+      type: 'Mały',
+      price: 10,
+    },
+    {
+      title: 'Napój',
+      type: 'Duży',
+      price: 15,
+    }
+  ];
 
   constructor() { }
 
