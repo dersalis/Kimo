fc4e90bc707263db40a08bde04defdf07856a279
Damian Ruta <damian.ruta@hotmail.com>
2021-02-22 22:01:12 +0100

    Models

---
+++ api/Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace api.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set;}
+        public string Language { get; set; }
+    }
+}
