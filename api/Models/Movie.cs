c56eb706ebf0df7a0cf9cb471e3da9cfda3a09f2
Damian Ruta <damian.ruta@hotmail.com>
2021-03-10 19:16:39 +0100

    Walidacja modelu

--- api/Models/Movie.cs
+++ api/Models/Movie.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Http;
 
@@ -6,8 +7,11 @@ namespace api.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nazwa nie moze byc pusta")]
         public string Name { get; set;}
+        [Required(ErrorMessage = "Język nie moze byc pusty")]
         public string Language { get; set; }
+        [Required(ErrorMessage = "Ocena nie moze byc pusta")]
         public double Rating { get; set; }
         [NotMapped]
         public IFormFile Image { get; set; }
